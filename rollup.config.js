d00156e11ff49525d757a2dd034fdaf968cb5767
Jan Wilhelm <jan.b.h.wilhelm@gmx.de>
2024-06-07 09:59:47 +0200

    adapt package resolving code

--- rollup.config.js
+++ rollup.config.js
@@ -2,12 +2,14 @@ import json from '@rollup/plugin-json';
 import terser from '@rollup/plugin-terser';
 import typescript from '@rollup/plugin-typescript';
 import path from 'path';
-import pkg from './package.json' with { type: 'json' };
+import fs from 'fs';
 
-const rootPackagePath = path.resolve(import.meta.dirname, '../');
+const rootPackagePath = process.cwd();
+const pkgPath = path.join(rootPackagePath, 'package.json');
+const pkg = JSON.parse(fs.readFileSync(pkgPath));
 
 const outputDir = path.join(rootPackagePath, 'lib');
-const pgkName = pkg.name.split('/').pop();
+const pgkName = pkg.name.split('-').pop();
 
 const external = [...Object.keys(pkg.dependencies || {}), ...Object.keys(pkg.peerDependencies || {})];
 
